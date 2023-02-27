cfcfd9dbb667815fdf373b8d5477af1ec7fdc0ae
Jovid <jovid.9@gmail.com>
2023-02-28 00:01:24 +0500

    jwt creation tested

--- test/api/test_user.py
+++ test/api/test_user.py
@@ -12,6 +12,28 @@ def test_register_user():
     )
     response = client.post("/auth/users/", payload)
     data = response.data
+    status = response.status_code
 
+    assert status == 201
     assert data["phone"] == payload["phone"]
     assert "password" not in data
+
+
+@pytest.mark.django_db
+def test_create_jwt():
+    payload = dict(
+        phone= "111919191",
+        password= "pass@123",
+        re_password= "pass@123",
+    )
+    response = client.post("/auth/users/", payload)
+
+    pyaload_jwt = dict(
+        phone = payload["phone"],
+        password = payload["password"]
+
+    )
+    response_jwt = client.post("/auth/jwt/create/", pyaload_jwt)
+    status_jwt = response_jwt.status_code
+
+    assert status_jwt == 200
